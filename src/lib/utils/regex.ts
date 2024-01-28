e35302e7a006a2074f81cb6ff2b8a248baefb149
Aromal Anil <aromalanilkannan@gmail.com>
2024-01-29 01:14:03 +0530

    fix: improve result title regex

--- src/lib/utils/regex.ts
+++ src/lib/utils/regex.ts
@@ -2,4 +2,4 @@ export const priceDetailsRegex = /(1st|2nd|3rd|\dth|Cons)\s+Prize\s*-?\s*(Rs|₹
 export const ticketNumberInResultRegex =
   /^(\d\s*\)\s*)?(([A-Z]{2})\s)?(\d{6}|\d{4}|)(\s*\(\s*([A-Z]+)\)\s*)?$/;
 export const ticketNumberRegex = /^([A-Z]{2})\s([0-9]{6})$/;
-export const resultTitleRegex = /^([A-Z\s\-0-9]+)LOTTERY NO\.([A-Z]{2}-\d+)(th|st|nd) DRAW held on:-\s+(\d{2}\/\d{2}\/\d{4}),(\d{1,2}:\d{2} [AP]M)$/
+export const resultTitleRegex = /^([A-Z\s\-/'0-9]+)LOTTERY NO\.([A-Z]{2}-\d+)(th|st|nd) DRAW held on:-\s+(\d{2}\/\d{2}\/\d{4}),(\d{1,2}:\d{2} [AP]M)$/
